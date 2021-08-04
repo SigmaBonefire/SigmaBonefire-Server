e7b5b9d5659d85226d30412fcc7345aa1435cb9e
Id0lAnt <getdonee@protonmail.com>
2021-08-04 23:39:59 +0300

    testing

--- source/sigmaserver.cpp
+++ source/sigmaserver.cpp
@@ -13,5 +13,12 @@ int main()
     iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0)
         std::cout << "WSAStartup failed: " << iResult << std::endl;
-    std::cout << "Sigma" << std::endl;
-}
+    
+    struct addrinfo *result = NULL,
+                *ptr = NULL,
+                hints;
+
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
