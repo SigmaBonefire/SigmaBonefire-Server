718a01cf3f36d1d804476d25c97b5b5a059caad7
Id0lAnt <getdonee@protonmail.com>
2021-08-04 20:21:44 +0300

    linked ws2_32.lib library. added a .gitignore file.

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,17 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
 #include <iostream>
 
 int main()
 {
+    WSADATA wsaData;
+    
+    int iResult;
+
+    // initializing winsock.
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (iResult != 0)
+        std::cout << "WSAStartup failed: " << iResult << std::endl;
     std::cout << "Sigma" << std::endl;
 }
